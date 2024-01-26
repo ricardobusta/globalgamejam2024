137aec94a8ff55f178dba8c94181c0ce1c7fc981
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2024-01-26 23:39:28 +0100

    Added clicable area on popper

--- MakeMeLaugh/mini_games/party_poppers/scripts/game_controller.gd
+++ MakeMeLaugh/mini_games/party_poppers/scripts/game_controller.gd
@@ -2,6 +2,7 @@ extends Node
 
 @onready var party_popper_01 = $"../Party_popper_01/Party_popper_012D"
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,6 +11,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# if click on popper, it pops
-	if Input.is_action_pressed("mouse_button_left"):
+	
 		print("plim")
 		party_popper_01.texture = preload("res://mini_games/party_poppers/sprites/opened_popper_01.png")
