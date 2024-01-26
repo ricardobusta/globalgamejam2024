39ebc977d17c857e4eee09c0583c30ca7d414036
Ricardo Bustamante <ricardo@busta.dev>
2024-01-26 22:01:29 +0100

    make title screen go to gameplay scene. add test buttons scene.

--- MakeMeLaugh/meta/title_screen_controller.gd
+++ MakeMeLaugh/meta/title_screen_controller.gd
@@ -6,4 +6,4 @@ func _ready():
     play_button.pressed.connect(_play_button_pressed)
 
 func _play_button_pressed() -> void:
-    pass
+    get_tree().change_scene_to_file("res://meta/gameplay.tscn")
