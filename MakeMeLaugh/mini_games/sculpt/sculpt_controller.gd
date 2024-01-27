98a08e7fb24d43cecf9b01dcbbf3f12dc84d0b70
Ricardo Bustamante <ricardo@busta.dev>
2024-01-27 23:50:08 +0100

    make sculpt game

--- MakeMeLaugh/mini_games/sculpt/sculpt_controller.gd
+++ MakeMeLaugh/mini_games/sculpt/sculpt_controller.gd
@@ -1,7 +1,21 @@
 extends MinigameController
 
+@onready var clickable_area: ClickableArea3D = $Container/ClickableArea3D
+@onready var blocks: Node3D = $Container/Blocks
+
 func _ready() -> void:
-    print("a")
+    clickable_area.clicked.connect(_clicked)
 
 func timeout() -> void:
     pass
+
+func _clicked(_node: Node3D) -> void:
+    if _paused():
+        return
+
+    if blocks.get_child_count() > 0:
+        var child = blocks.get_child(0)
+        blocks.remove_child(child)
+        child.queue_free()
+        if blocks.get_child_count() == 0:
+            _win_game()
