4d1dcf618b2d078c739702bfae1d38eac8fdfa35
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2024-01-27 13:09:37 +0100

    a

--- MakeMeLaugh/mini_games/party_poppers/party_poppers_controller.gd
+++ MakeMeLaugh/mini_games/party_poppers/party_poppers_controller.gd
@@ -10,15 +10,15 @@ extends MinigameController
 @onready var party_popper_03_sprite:Sprite2D = $Party_popper_03/PartyPopperSprite
 
 func _ready() -> void:
-    party_popper_01.clicked.connect(_on_popper_clicked)
-    party_popper_02.clicked.connect(_on_popper_clicked)
-    party_popper_03.clicked.connect(_on_popper_clicked)
+	party_popper_01.clicked.connect(_on_popper_clicked)
+	party_popper_02.clicked.connect(_on_popper_clicked)
+	party_popper_03.clicked.connect(_on_popper_clicked)
 
 func _on_popper_clicked(popper: Popper) -> void:
-    popper.sprite.texture = preload("res://mini_games/party_poppers/sprites/opened_popper_01.png")
-    popper.popped = true
-    _all_popped()
+	popper.sprite.texture = preload("res://mini_games/party_poppers/sprites/opened_popper_01.png")
+	popper.popped = true
+	_all_popped()
 
 func _all_popped() -> void:
-    if party_popper_01.popped and party_popper_02.popped and party_popper_03.popped:
-        _win_game()
+	if party_popper_01.popped and party_popper_02.popped and party_popper_03.popped:
+		_win_game()
--- MakeMeLaugh/shared/clickable_area2d.gd
+++ MakeMeLaugh/shared/clickable_area2d.gd
@@ -5,10 +5,10 @@ class_name ClickableArea2D
 signal clicked
 
 func _ready() -> void:
-    input_event.connect(_on_input_event)
+	input_event.connect(_on_input_event)
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-    if event is InputEventMouseButton:
-        var button: InputEventMouseButton = event
-        if button.button_index == MOUSE_BUTTON_LEFT and button.pressed:
-            clicked.emit(self)
+	if event is InputEventMouseButton:
+		var button: InputEventMouseButton = event
+		if button.button_index == MOUSE_BUTTON_LEFT and button.pressed:
+			clicked.emit(self)
