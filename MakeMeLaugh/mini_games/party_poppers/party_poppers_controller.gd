65edcc1596ed4eb2d5db686e786cda1d3afea5c6
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2024-01-27 12:28:47 +0100

    Add party popper victory condition

--- MakeMeLaugh/mini_games/party_poppers/party_poppers_controller.gd
+++ MakeMeLaugh/mini_games/party_poppers/party_poppers_controller.gd
@@ -1,11 +1,28 @@
 extends MinigameController
 
-@onready var party_popper_01 = $"Party_popper_01/Party_popper_012D"
+@onready var party_popper_01: = $Party_popper_01/Popper01Area2D
+@onready var party_popper_01_sprite:Sprite2D = $Party_popper_01/PartyPopperSprite
+
+@onready var party_popper_02:Popper = $Party_popper_02/Popper02Area2D
+@onready var party_popper_02_sprite:Sprite2D = $Party_popper_02/PartyPopperSprite
+
+@onready var party_popper_03:Popper = $Party_popper_03/Popper03Area2D
+@onready var party_popper_03_sprite:Sprite2D = $Party_popper_03/PartyPopperSprite
 
 func _ready():
-    pass # Replace with function body.
+	party_popper_01.clicked.connect(_on_popper_clicked)
+	party_popper_02.clicked.connect(_on_popper_clicked)
+	party_popper_03.clicked.connect(_on_popper_clicked)
 
 func _process(delta):
-    # if click on popper, it pops
-        print("plim")
-        party_popper_01.texture = preload("res://mini_games/party_poppers/sprites/opened_popper_01.png")
+	pass
+
+func _on_popper_clicked(popper: Popper) -> void:
+	print(popper.name)
+	popper.sprite.texture = preload("res://mini_games/party_poppers/sprites/opened_popper_01.png")
+	popper.popped = true
+	_all_popped()
+	
+func _all_popped():
+	if party_popper_01.popped and party_popper_02.popped and party_popper_03.popped:
+		_win_game()
