d8296f4ee365c5c193638fa5f251e7f6b2189fb3
Ricardo Bustamante <ricardo@busta.dev>
2024-01-28 12:13:00 +0100

    add comedic twist to parking cars

--- MakeMeLaugh/mini_games/parallel_parking/parallel_parking_controller.gd
+++ MakeMeLaugh/mini_games/parallel_parking/parallel_parking_controller.gd
@@ -2,6 +2,12 @@ extends MinigameController
 
 @onready var car:CarController = $CarArea2D
 
+@onready var car2: Node2D = $Car2
+@onready var car3: Node2D = $Car3
+@onready var car4: Node2D = $Car4
+@onready var car5: Node2D = $Car5
+@onready var car6: Node2D = $Car6
+
 func _ready():
     car.car_crashed.connect(_car_crashed)
     car.car_parked.connect(_car_parked)
@@ -10,6 +16,11 @@ func _car_crashed():
     _lose_game()
 
 func _car_parked():
+    _move_car(car2, 1000)
+    _move_car(car3, -1000)
+    _move_car(car4, -1000)
+    _move_car(car5, 1000)
+    _move_car(car6, 1000)
     _win_game()
 
 func timeout() -> void:
@@ -20,3 +31,7 @@ func _process(_delta: float) -> void:
         car.is_car_on = false
         return
     car.is_car_on = true
+
+func _move_car(c: Node2D, a: float) -> void:
+    var tween = get_tree().create_tween()
+    tween.tween_property(c, "position:y", c.position.y+a, 1.0)
