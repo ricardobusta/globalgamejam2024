c2b1a70a545f031a654a90686dec0471707a776e
Ricardo Bustamante <ricardo@busta.dev>
2024-01-28 11:58:02 +0100

    wrap up sculpt game

--- MakeMeLaugh/mini_games/sculpt/sculpt_controller.gd
+++ MakeMeLaugh/mini_games/sculpt/sculpt_controller.gd
@@ -3,6 +3,8 @@ extends MinigameController
 @onready var clickable_area: ClickableArea3D = $Container/ClickableArea3D
 @onready var blocks: Node3D = $Container/Blocks
 @onready var audio_stream: AudioStreamPlayer = $AudioStreamPlayer
+@onready var directional_light: Node3D = $DirectionalLight3D
+@onready var spot_light: Node3D = $SpotLight3D
 
 const break_sound: AudioStream = preload("res://mini_games/sculpt/sfx/break.wav")
 const tada_sound: AudioStream = preload("res://mini_games/sculpt/sfx/tada.wav")
@@ -24,10 +26,12 @@ func _clicked(_node: Node3D) -> void:
         audio_stream.stream = break_sound
         audio_stream.play()
         if blocks.get_child_count() == 0:
+            directional_light.hide()
+            spot_light.show()
             var tween = get_tree().create_tween()
-            tween.tween_property($Camera3D, "position", Vector3(0,-1.131,1.913), 0.3)
+            tween.tween_property($Camera3D, "position", Vector3(0.291,0.802,1.661), 0.3)
             var tween2 = get_tree().create_tween()
-            tween2.tween_property($Camera3D, "rotation", Vector3(deg_to_rad(34.2),0,0), 0.3)
+            tween2.tween_property($Camera3D, "rotation", Vector3(deg_to_rad(-14.9),deg_to_rad(6.4),deg_to_rad(-0.4)), 0.3)
             get_tree().create_timer(0.5).timeout.connect(_play_tada)
             _win_game()
 
